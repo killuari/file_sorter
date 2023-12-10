c75650758b112256ffdce7b96edf15c030f03ca3
luis <killuari@protonmail.com>
2023-12-10 17:27:09 +0100

    added a setup
    setup creates the default config and all the folders needed

---
+++ setup.py
@@ -0,0 +1,25 @@
+import config
+import os
+
+config.create_default_config()
+
+config.load_config()
+sorting_path = config.get_sorting_path()
+folders = config.load_folders()
+
+try:
+    os.mkdir(sorting_path)
+    print("Documents Folder created")
+except FileNotFoundError:
+    print("Documents Folder couldn't be located")
+except FileExistsError:
+    print("Sorted Folder already exists")
+
+for folder in folders:
+    try:
+        os.mkdir(folders[folder])
+        print(f"{folder} Folder created")
+    except FileNotFoundError:
+        print("Documents or Sorted Folder couldn't be located")
+    except FileExistsError:
+        print(f"{folder} Folder already exists")
